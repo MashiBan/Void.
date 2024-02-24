a98ee05d73bb9a8995d4b05ef9996d8856fd14e3
Manashi Banerjee <mashibann@Manashis-MacBook-Air.local>
2024-02-24 15:01:33 +0530

    first commit

---
+++ src/messages.js
@@ -0,0 +1,112 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import waterVertexShader from './shaders/messages/vertex.glsl'
+import waterFragmentShader from './shaders/messages/fragment.glsl'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Water
+ */
+// Geometry
+const waterGeometry = new THREE.PlaneGeometry(2, 2, 512, 512)
+
+// Material
+const waterMaterial = new THREE.ShaderMaterial({
+    vertexShader: waterVertexShader,
+    fragmentShader: waterFragmentShader,
+    uniforms: {
+        iResolution: { value: new THREE.Vector3() },
+        iTime: { value: 0 },
+        iTimeDelta: { value: 0 },
+        iFrameRate: { value: 0 },
+        iFrame: { value: 0 },
+        iChannelTime: { value: [0, 0, 0, 0] },
+        iChannelResolution: { value: [new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3()] },
+        iMouse: { value: new THREE.Vector4() },
+        iChannel0: { value: null },
+        iChannel1: { value: null },
+        iChannel2: { value: null },
+        iChannel3: { value: null },
+        iDate: { value: new THREE.Vector4() },
+        iSampleRate: { value: 44100 }
+    }
+})
+
+// Mesh
+const water = new THREE.Mesh(waterGeometry, waterMaterial)
+water.rotation.x = - Math.PI * 0.5
+scene.add(water)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(1, 1, 1)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update water shader uniforms
+    waterMaterial.uniforms.iResolution.value.set(sizes.width, sizes.height, 1)
+    waterMaterial.uniforms.iTime.value = elapsedTime
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
